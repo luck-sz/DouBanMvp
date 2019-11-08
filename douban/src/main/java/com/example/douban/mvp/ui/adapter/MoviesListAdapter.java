bbb0147d945727e77132b36a5f399263945a43ee
1037055363@qq.com <1037055363@qq.com>
2019-11-08 17:20:16 +0800

    1.修电影榜单Item

--- douban/src/main/java/com/example/douban/mvp/ui/adapter/MoviesListAdapter.java
+++ douban/src/main/java/com/example/douban/mvp/ui/adapter/MoviesListAdapter.java
@@ -22,6 +22,9 @@ public class MoviesListAdapter extends BaseQuickAdapter<MovieListBean, BaseViewH
     protected void convert(@NonNull BaseViewHolder helper, MovieListBean item) {
         helper.setText(R.id.tv_movies_list_title, item.getTitle());
         helper.setText(R.id.tv_movie_tip, "每周五更新·共" + item.getSize() + "部");
+        helper.setText(R.id.tv_no_1, "1." + item.getMovies().get(0).getMovie_name());
+        helper.setText(R.id.tv_no_2, "2." + item.getMovies().get(1).getMovie_name());
+        helper.setText(R.id.tv_no_3, "3." + item.getMovies().get(2).getMovie_name());
         ArmsUtils.obtainAppComponentFromContext(mContext)
                 .imageLoader()
                 .loadImage(mContext, ImageConfigImpl
